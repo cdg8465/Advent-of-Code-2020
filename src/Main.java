552c96fba98804646f006cd520c9046d6fa46782
cdg8465 <cdg8465@rit.edu>
2021-09-23 19:55:50 -0400

    Completed day 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Puzzle2 day2 = new Puzzle2();
-        System.out.println(day2.numOfValidPasswords());
+        System.out.println(day2.numOfValidPasswordsNewRules());
     }
 }
--- src/Puzzle2.java
+++ src/Puzzle2.java
@@ -11,26 +11,9 @@ public class Puzzle2 {
         try {
             Scanner scanner = new Scanner(entries);
 
-            while(scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-
-                // Get full line of arguments
-                String[] args = line.split(" ");
-
-                // Get min and max
-                String[] minAndMax = args[0].split("-");
-                int min = Integer.parseInt(minAndMax[0]);
-                int max = Integer.parseInt(minAndMax[1]);
-
-                // Get letter
-                String letter = args[1].substring(0, 1);
-
-                // Get password
-                String password = args[2];
-
-                if(checkPassword(password, letter, min, max))
+            while(scanner.hasNextLine())
+                if(checkPassword(parseArgs(scanner.nextLine())))
                     count++;
-            }
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
         }
@@ -38,13 +21,45 @@ public class Puzzle2 {
         return count;
     }
 
-    private boolean checkPassword(String password, String letter, int min, int max) {
+    private String[] parseArgs(String line) {
+        // Get full line of arguments
+        String[] args = line.split(" ");
+
+        // Get hyphenated numbers
+        String[] hyphenated = args[0].split("-");
+
+        return new String[]{args[2], args[1].substring(0, 1), hyphenated[0], hyphenated[1]};
+    }
+
+    public int numOfValidPasswordsNewRules() {
         int count = 0;
 
-        for(int i = 0; i < password.length(); i++)
-            if(("" + password.charAt(i)).equals(letter))
+        try {
+            Scanner scanner = new Scanner(entries);
+
+            while(scanner.hasNextLine())
+                if(checkPassword2(parseArgs(scanner.nextLine())))
+                    count++;
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+
+        return count;
+    }
+
+    private boolean checkPassword(String[] args) {
+        int count = 0;
+
+        for(int i = 0; i < args[0].length(); i++)
+            if(("" + args[0].charAt(i)).equals(args[1]))
                 count++;
 
-        return count >= min && count <= max;
+        return count >= Integer.parseInt(args[2]) && count <= Integer.parseInt(args[3]);
+    }
+
+    private boolean checkPassword2(String[] args) {
+        if(("" + args[0].charAt(Integer.parseInt(args[2]) - 1)).equals(args[1]) && !("" + args[0].charAt(Integer.parseInt(args[3]) - 1)).equals(args[1]))
+            return true;
+        else return ("" + args[0].charAt(Integer.parseInt(args[3]) - 1)).equals(args[1]) && !("" + args[0].charAt(Integer.parseInt(args[2]) - 1)).equals(args[1]);
     }
 }
